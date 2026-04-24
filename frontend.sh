a7efe678ce439cbb510ca49cc77d66610e14588b
satishbabu <satishbabu.uppula14@gmail.com>
2026-04-24 11:44:43 +0530

    2nd step added remove directory command which cleans the previous frontend directory

--- frontend.sh
+++ frontend.sh
@@ -5,6 +5,7 @@ curl -fsSL https://rpm.nodesource.com/setup_20.x | bash -
 dnf install -y nodejs
 
 curl -L -o /tmp/frontend.zip https://raw.githubusercontent.com/raghudevopsb89/roboshop-microservices/main/artifacts/frontend.zip
+rm -rf /tmp/frontend
 mkdir -p /tmp/frontend
 cd /tmp/frontend
 unzip /tmp/frontend.zip
